5a54e184fc91e774e5803c6f7877329b44131c82
Rafael7 <rafaeldeandradealbuquerque7@gmail.com>
2024-12-22 21:39:45 -0300

    finalização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms'; 
+
+interface User {
+  id?: number;
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'poc-nava-frontend';
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
+  users: User[] = [];
+  newUser: User = { name: '', email: '' };
+  editMode: boolean = false;
+  selectedUserId: number | null = null;
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.http.get<User[]>('https://localhost:4001/api/User').subscribe((data) => {
+      this.users = data;
+    });
+  }
+
+  saveUser(): void {
+    if (this.editMode && this.selectedUserId !== null) {
+      this.newUser.id = this.selectedUserId;
+      this.http
+        .put(`https://localhost:4001/api/User/${this.selectedUserId}`, this.newUser)
+        .subscribe(() => {
+          this.loadUsers();
+          this.resetForm();
+        });
+    } else {
+      this.http.post('https://localhost:4001/api/User', this.newUser).subscribe(() => {
+        this.loadUsers();
+        this.resetForm();
+      });
+    }
+  }
+
+  editUser(user: User): void {
+    this.newUser = { name: user.name, email: user.email };
+    this.selectedUserId = user.id || null;
+    this.editMode = true;
+  }
+
+  deleteUser(id: number | undefined): void {
+    if (id === undefined) {
+      console.error('ID is undefined. Cannot delete user.');
+      return;
+    }
+  
+    this.http.delete(`https://localhost:4001/api/User/${id}`).subscribe(() => {
+      this.loadUsers();
+    });
+  }
+  
+
+  resetForm(): void {
+    this.newUser = { name: '', email: '' };
+    this.editMode = false;
+    this.selectedUserId = null;
+  }
 }
